9e703504cb92e44905b08a994f7ab5494d0ca066
AKdimka <dimabrigadir1@gmail.com>
2022-04-29 10:48:13 +0300

    ?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,8 @@
+import { AppBar } from "./AppBar/AppBar";
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+	return (
+		<div>
+			<AppBar />
+		</div>
+	);
 };
